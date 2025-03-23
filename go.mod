c43f1fd7c5d5ce435f654822f66c162b3079ed88
Ryotaro Oda <plm23.ccc@gmail.com>
2025-03-23 16:47:50 +0900

    fix: saml example

--- go.mod
+++ go.mod
@@ -4,14 +4,22 @@ go 1.24.1
 
 require (
 	github.com/coreos/go-oidc/v3 v3.13.0
+	github.com/crewjam/saml v0.4.14
 	github.com/go-ldap/ldap/v3 v3.4.10
 	golang.org/x/oauth2 v0.28.0
 )
 
 require (
 	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
+	github.com/beevik/etree v1.1.0 // indirect
+	github.com/crewjam/httperr v0.2.0 // indirect
 	github.com/go-asn1-ber/asn1-ber v1.5.7 // indirect
 	github.com/go-jose/go-jose/v4 v4.0.5 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/mattermost/xml-roundtrip-validator v0.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russellhaering/goxmldsig v1.3.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 )
---
+++ saml-sp/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/crewjam/saml/samlsp"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	s := samlsp.SessionFromContext(r.Context())
+	if s == nil {
+		return
+	}
+	sa, ok := s.(samlsp.SessionWithAttributes)
+	if !ok {
+		return
+	}
+	fmt.Fprintf(w, "UserInfo\n")
+	as := sa.GetAttributes()
+	for k, v := range as {
+		fmt.Printf("key: %s value: %s\n", k, v)
+		fmt.Fprintf(w, "key: %s value: %s\n", k, v)
+	}
+}
+
+func main() {
+	confIDPMetadataURL := "http://localhost:18080/realms/master/protocol/saml/descriptor"
+	rootURL, err := url.Parse("http://localhost:3000")
+	if err != nil {
+		log.Fatalf("failed to parse root URL: %v", err)
+	}
+
+	keyPair, err := tls.LoadX509KeyPair("cert.pem", "key.pem")
+	if err != nil {
+		panic(err)
+	}
+	keyPair.Leaf, err = x509.ParseCertificate(keyPair.Certificate[0])
+	if err != nil {
+		panic(err)
+	}
+	idpMetadataURL, err := url.Parse(confIDPMetadataURL)
+	if err != nil {
+		panic(err)
+	}
+	idpMetadata, err := samlsp.FetchMetadata(context.Background(), http.DefaultClient, *idpMetadataURL)
+	if err != nil {
+		panic(err)
+	}
+	samlSP, err := samlsp.New(samlsp.Options{
+		EntityID:    "http://localhost:3000/saml/metadata",
+		URL:         *rootURL,
+		Key:         keyPair.PrivateKey.(*rsa.PrivateKey),
+		Certificate: keyPair.Leaf,
+		IDPMetadata: idpMetadata,
+		SignRequest: true,
+	})
+	if err != nil {
+		log.Fatalf("failed to create samlsp: %v", err)
+	}
+	app := http.HandlerFunc(hello)
+	http.Handle("/hello", samlSP.RequireAccount(app))
+	http.Handle("/saml/", samlSP)
+	http.ListenAndServe(":3000", nil)
+}
+
+func loadPrivateKey(path string) *rsa.PrivateKey {
+	data, _ := os.ReadFile(path)
+	block, _ := pem.Decode(data)
+	key, _ := x509.ParsePKCS1PrivateKey(block.Bytes)
+	return key
+}
